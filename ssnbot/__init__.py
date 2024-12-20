f608a4fbda788a23722dc71d99ca350427113dd4
Why You Visit My Profile 😂 <153857610+prf100rabhAaBb40769374divyanshi40769374@users.noreply.github.com>
2024-12-20 20:51:36 +0530

    [ 𝗣𝗥𝚯𝗙𝗘𝗦𝗦𝚯𝗥 ][ ѕαиαтαиι ]

--- ssnbot/__init__.py
+++ ssnbot/__init__.py
@@ -9,12 +9,12 @@ load_dotenv(override=True)
 pattern = re.compile(r"^.\d+$")
 
 # vars
-APP_ID = os.environ.get("APP_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-DB_URL = os.environ.get("DB_URL", "")
-OWNER_ID = int(os.environ.get('OWNER_ID', ""))
-MUST_JOIN = os.environ.get("MUST_JOIN", "")
+APP_ID = os.environ.get("APP_ID", "26598255")
+API_HASH = os.environ.get("API_HASH", "2851fb6ab1bdcd0fcaf768dfcb923ffb")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7575561551:AAF4FpCmHFKD6dTfXXscoPihbpOnwdD-HbQ")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = int(os.environ.get('OWNER_ID', "7400823450"))
+MUST_JOIN = os.environ.get("MUST_JOIN", "PROFESSORxBOTS")
 ADMINS = [
     int(user) if pattern.search(user) else user
     for user in os.environ.get("ADMINS", "").split()
